ddeb88f35e53647e20a34ce6a3a3858dc6b2319c
Goken5 <fdavipessanha@gmail.com>
2025-10-21 15:03:45 -0300

    poucas alterações no LoginDiv

--- frontend/src/components/LoginDiv.tsx
+++ frontend/src/components/LoginDiv.tsx
@@ -7,12 +7,13 @@ type dados = {
 }
 export default function Login(){
     return(
-        <form className=" justify-center border-blue-600 bg-blue-300 p-5 rounded-3xl ">
+        <form className=" text-center justify-center border-blue-600 bg-blue-300 p-5 rounded-3xl  ">
             <label htmlFor="email" className="block text-black">Email:</label>
-            <input type="text" className="block text-black border-black border-1 transition-all focus:border-3 bg-neutral-50 rounded-2xl"/>
+            <input type="text" className="block text-black border-black border-1 transition-all focus:scale-105 focus:border-2 bg-neutral-50 rounded-2xl w-full"/>
             <label htmlFor="nome" className="block text-black">Nome</label>
-            <input type="text" className="block text-black border-black border-1 transition-all focus:border-3 bg-neutral-50 rounded-2xl"/>
-
+            <input type="text" className="block text-black border-black border-1 transition-all focus:scale-105 focus:border-2 bg-neutral-50 rounded-2xl w-full"/>
+            <label htmlFor="nome" className="block text-black">Senha</label>
+            <input type="password" className="block text-black border-black border-1 transition-all focus:scale-105 focus:border-2 bg-neutral-50 rounded-2xl w-full"/>
         </form>
     )
 }
