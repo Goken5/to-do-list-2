704af0e90473f9b3bb7f19cde89078c92e1919f5
Goken5 <fdavipessanha@gmail.com>
2025-10-23 10:44:15 -0300

    comecei a colocar o post no Registro

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,7 +6,9 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
     res.send("Servidor ligado");
 })
-
+app.post("/register", (req: Request, res: Response) => {
+    req.params
+} )
 app.listen(8000, () => {
     console.log("Servidor rodando na porta 8000");
 });
--- frontend/src/components/LoginDiv.tsx
+++ frontend/src/components/LoginDiv.tsx
@@ -1,13 +1,12 @@
 import { useNavigate } from "react-router-dom";
+import { useState, type FormEvent } from "react";
 import Input from "./Input"
+import axios from "axios";
 
-type dados = {
-    nome: string;
-    email: string;
-    senha: string;
-}
+
 export function Login(){
     const navigate = useNavigate();
+    
     return(
         <form className=" flex flex-col text-center justify-center border-blue-600 border-2 bg-blue-300 sm:w-[40vw] w-[90vw] h-[70vh] rounded-3xl items-center ">
             <h1 className="text-4xl text-white font-bold mb-5">Bem Vindo!</h1>
@@ -29,19 +28,40 @@ export function Login(){
     )
 }
 export function SignUp(){
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [confirmar, setConfirmar] = useState("");
+    const [error, setError] = useState("");
+
+    const handleClick = async(e: FormEvent) => {
+        e.preventDefault()
+        if(senha != confirmar){
+            alert("As senhas não coincidem");
+            return;
+        }
+        console.log("Recebi os crias: " + nome + email + senha + confirmar);
+        await axios.post("http://localhost:8000", {
+            "nome": nome, 
+            "email": email,
+            "senha": senha,
+        });
+    }
+
     return(
         <form className=" flex flex-col text-center justify-center border-blue-600 border-2 bg-blue-300 sm:w-[70vw] w-[90vw] h-[70vh] rounded-3xl items-center">
             <h1 className="text-4xl text-white font-bold mb-5">Registre-se no To-do List</h1>
             <h2 className="text-3xl text-white font-semibold mb-10">Registre-se no To-do List e comece a organizar suas ideias <strong>hoje</strong></h2>
-            <Input type="text" placeholder="Nome"/>
-            <Input type="email" placeholder="E-mail"/>
-            <Input type="password" placeholder="Senha" />
-            <Input type="passoword" placeholder="Confirmar Senha" />
+            <Input type="text" placeholder="Nome" onChange={(e) => setNome(e.target.value)}/>
+            <Input type="email" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)}/>
+            <Input type="password" placeholder="Senha" onChange={(e) => setSenha(e.target.value)} />
+            <Input type="password" placeholder="Confirmar Senha" onChange={(e) => setConfirmar(e.target.value)}/>
             <button type="submit" className="
             bg-blue-700 rounded-4xl border-black border-2 p-3 pr-20 pl-20
               hover:scale-110 hover:cursor-pointer transition-all
             text-white hover:text-black hover:shadow-2xl
-            hover:shadow-black mb-3">Registrar</button>
+            hover:shadow-black mb-3"
+            onClick={handleClick}>Registrar</button>
         </form>
     )
 }
