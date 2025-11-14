35e6b9c7c22da198652255193af6ed1e27d755a6
Goken5 <fdavipessanha@gmail.com>
2025-11-14 11:53:49 -0300

    deu ruim

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,13 +18,6 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization", "x-requested-with"]
 }));
 
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-requested-with");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.sendStatus(200);
-});
 
 app.use(express.json());
 
